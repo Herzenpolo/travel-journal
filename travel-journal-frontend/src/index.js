27bd274eab21bfa00627fc368249edbc8abf071d
Herzen Cortes <herzencc@icloud.com>
2020-05-28 16:43:30 -0400

    testing + DB pop

--- travel-journal-frontend/src/index.js
+++ travel-journal-frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/fonts/Hero-Regular.ttf'
+import './components/fonts/Perfograma-Regular.ttf'
 
 
 
